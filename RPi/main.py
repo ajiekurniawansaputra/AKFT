80abdb564d19c6e563f95e44af5d0195e8eaee53
Ajie Kurniawan Saputra <AjieKurniawanSaputra@Gmail.com>
2022-06-13 22:14:25 +0700

    backround thread now inside while loop to make sure it will run again if error happened

--- RPi/main.py
+++ RPi/main.py
@@ -6,7 +6,6 @@ import fingerprint
 import pincam
 import rfid
 import threading
-import time
 import logging
 import serial
 
@@ -31,20 +30,24 @@ def main(debug=False):
     
 #add error handling to background
 def background():
-    logging.debug('Background thread start')
-    client = util.client
-    client.message_callback_add('SGLCERIC/auth/rfid/'+str(util.this_room.id), nfc.response)
-    client.message_callback_add('SGLCERIC/sync/add/'+str(util.this_room.id), fp.add)
-    client.message_callback_add('SGLCERIC/sync/del/'+str(util.this_room.id), fp.delete)
-    client.message_callback_add('SGLCERIC/open', util.door_command)
-    client.message_callback_add('SGLCERIC/pass/'+str(util.this_room.id), pincam.password_command)
-    client.will_set(topic='SGLCERIC/connection/'+str(util.this_room.id), payload='Device Lost Connection', qos=1, retain=True )
-    client.on_connect = on_connect
-    client.on_disconnect = on_disconnect
-    #client.tls_set('/home/pi/Documents/client/ca.crt', '/home/pi/Documents/client/client.crt', '/home/pi/Documents/client/client.key')
-    #client.connect('54.196.200.69', 8883)
-    client.connect('broker.hivemq.com', 1883, 10)
-    client.loop_forever()
+    while True:
+        try:
+            logging.debug('Background thread start')
+            client = util.client
+            client.message_callback_add('SGLCERIC/auth/rfid/'+str(util.this_room.id), nfc.response)
+            client.message_callback_add('SGLCERIC/sync/add/'+str(util.this_room.id), fp.add)
+            client.message_callback_add('SGLCERIC/sync/del/'+str(util.this_room.id), fp.delete)
+            client.message_callback_add('SGLCERIC/open', util.door_command)
+            client.message_callback_add('SGLCERIC/pass/'+str(util.this_room.id), pincam.password_command)
+            client.will_set(topic='SGLCERIC/connection/'+str(util.this_room.id), payload='Device Lost Connection', qos=1, retain=True )
+            client.on_connect = on_connect
+            client.on_disconnect = on_disconnect
+            #client.tls_set('/home/pi/Documents/client/ca.crt', '/home/pi/Documents/client/client.crt', '/home/pi/Documents/client/client.key')
+            client.connect('127.0.0.1', 8883)
+            #client.connect('broker.hivemq.com', 1883, 10)
+            client.loop_forever()
+        except Exception as e:
+            logging.debug('background thread failed, restarting', e)
 
 def on_connect(client, userdata, flags, rc):
     if rc==0:
